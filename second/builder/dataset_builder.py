1edbad921610a778a88629ea2a2f495d5f615a49
Pranjali Ajay Parse <pranjaliparse@gmail.com>
2021-05-11 00:52:29 -0400

    Add script changes

--- second/builder/dataset_builder.py
+++ second/builder/dataset_builder.py
@@ -1,75 +1,47 @@
-# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Input reader builder.
-
-Creates data sources for DetectionModels from an InputReader config. See
-input_reader.proto for options.
-
-Note: If users wishes to also use their own InputReaders with the Object
-Detection configuration framework, they should define their own builder function
-that wraps the build function.
-"""
-
+import numpy as np
+from functools import partial
 from second.protos import input_reader_pb2
 from second.data.dataset import get_dataset_class
 from second.data.preprocess import prep_pointcloud
 from second.core import box_np_ops
-import numpy as np
 from second.builder import dbsampler_builder
-from functools import partial
 from second.utils.config_tool import get_downsample_factor
 
-def build(input_reader_config,
-          model_config,
-          training,
-          voxel_generator,
-          target_assigner,
-          multi_gpu=False):
-    """Builds a tensor dictionary based on the InputReader config.
 
+"""Builder - Input Reader.
+
+Using an InputReader config, it creates data sources for DetectionModels.
+"""
+
+def build(input_reader_config, model_config, training, voxel_generator, target_assigner, multi_gpu = False):
+    """Based on the InputReader config, it builds a tensor dictionary b
     Args:
         input_reader_config: A input_reader_pb2.InputReader object.
 
     Returns:
-        A tensor dict based on the input_reader_config.
-
-    Raises:
-        ValueError: On invalid input reader proto.
-        ValueError: If no input paths are specified.
+        Based on the input_reader_config, it returns a tensor dict.
     """
-    if not isinstance(input_reader_config, input_reader_pb2.InputReader):
-        raise ValueError('input_reader_config not of type '
-                         'input_reader_pb2.InputReader.')
-    prep_cfg = input_reader_config.preprocess
-    dataset_cfg = input_reader_config.dataset
-    num_point_features = model_config.num_point_features
-    out_size_factor = get_downsample_factor(model_config)
+
+    prep_cfg = input_reader_config.preprocess #Prep config
+    dataset_cfg = input_reader_config.dataset #Dataset config
+    num_point_features = model_config.num_point_features #Number of point features
+    out_size_factor = get_downsample_factor(model_config) #Output size factor
+
     assert out_size_factor > 0
+
     cfg = input_reader_config
-    db_sampler_cfg = prep_cfg.database_sampler
-    db_sampler = None
-    if len(db_sampler_cfg.sample_groups) > 0 or db_sampler_cfg.database_info_path != "":  # enable sample
-        db_sampler = dbsampler_builder.build(db_sampler_cfg)
-    grid_size = voxel_generator.grid_size
-    feature_map_size = grid_size[:2] // out_size_factor
-    feature_map_size = [*feature_map_size, 1][::-1]
-    print("feature_map_size", feature_map_size)
-    assert all([n != '' for n in target_assigner.classes]), "you must specify class_name in anchor_generators."
+    db_sampler_cfg, db_sampler = prep_cfg.database_sampler, None
+
+    if len(db_sampler_cfg.sample_groups) > 0 or db_sampler_cfg.database_info_path != "": db_sampler = dbsampler_builder.build(db_sampler_cfg)
+
+    grid_size, feature_map_size, feature_map_size = voxel_generator.grid_size, grid_size[:2], [*feature_map_size, 1][::-1]
+    print("Feature Map Size", feature_map_size)
+
+    assert all([n != '' for n in target_assigner.classes]), "Specify class_name in anchor_generators."
     dataset_cls = get_dataset_class(dataset_cfg.dataset_class_name)
-    assert dataset_cls.NumPointFeatures >= 3, "you must set this to correct value"
-    assert dataset_cls.NumPointFeatures == num_point_features, "currently you need keep them same"
+    assert dataset_cls.NumPointFeatures >= 3, "Must set this to correct value"
+    assert dataset_cls.NumPointFeatures == num_point_features, "Keep num_point_features same"
+
     prep_func = partial(
         prep_pointcloud,
         root_path=dataset_cfg.kitti_root_path,
@@ -105,19 +77,14 @@ def build(input_reader_config,
 
     ret = target_assigner.generate_anchors(feature_map_size)
     class_names = target_assigner.classes
-    anchors_dict = target_assigner.generate_anchors_dict(feature_map_size)
-    anchors_list = []
-    for k, v in anchors_dict.items():
-        anchors_list.append(v["anchors"])
+    anchors_dict, anchors_list = target_assigner.generate_anchors_dict(feature_map_size), []
+    for k, value in anchors_dict.items(): anchors_list.append(value["anchors"])
     
-    # anchors = ret["anchors"]
-    anchors = np.concatenate(anchors_list, axis=0)
-    anchors = anchors.reshape([-1, target_assigner.box_ndim])
+    anchors = np.concatenate(anchors_list, axis=0).reshape([-1, target_assigner.box_ndim])
     assert np.allclose(anchors, ret["anchors"].reshape(-1, target_assigner.box_ndim))
-    matched_thresholds = ret["matched_thresholds"]
-    unmatched_thresholds = ret["unmatched_thresholds"]
-    anchors_bv = box_np_ops.rbbox2d_to_near_bbox(
-        anchors[:, [0, 1, 3, 4, 6]])
+    matched_thresholds, unmatched_thresholds = ret["matched_thresholds"], ret["unmatched_thresholds"]
+
+    anchors_bv = box_np_ops.rbbox2d_to_near_bbox(anchors[:, [0, 1, 3, 4, 6]])
     anchor_cache = {
         "anchors": anchors,
         "anchors_bv": anchors_bv,
@@ -125,6 +92,7 @@ def build(input_reader_config,
         "unmatched_thresholds": unmatched_thresholds,
         "anchors_dict": anchors_dict,
     }
+
     prep_func = partial(prep_func, anchor_cache=anchor_cache)
     dataset = dataset_cls(
         info_path=dataset_cfg.kitti_info_path,
--- second/builder/dbsampler_builder.py
+++ second/builder/dbsampler_builder.py
@@ -1,27 +1,27 @@
 import pickle
-
 import second.core.preprocess as prep
 from second.builder import preprocess_builder
-from second.core.preprocess import DataBasePreprocessor
 from second.core.sample_ops import DataBaseSamplerV2
+from second.core.preprocess import DataBasePreprocessor
 
-
+"""
+Database sampler builder given the sampler configuartion
+"""
 def build(sampler_config):
     cfg = sampler_config
-    groups = list(cfg.sample_groups)
-    prepors = [
-        preprocess_builder.build_db_preprocess(c)
-        for c in cfg.database_prep_steps
-    ]
+    rate = cfg.rate # DB Sample builder rate
+    groups = list(cfg.sample_groups) # Building sample groups
+    info_path = cfg.database_info_path # Info path
+    prepors = [preprocess_builder.build_db_preprocess(cf_g) for cf_g in cfg.database_prep_steps]
     db_prepor = DataBasePreprocessor(prepors)
-    rate = cfg.rate
+    
     grot_range = cfg.global_random_rotation_range_per_object
-    groups = [dict(g.name_to_max_num) for g in groups]
-    info_path = cfg.database_info_path
-    with open(info_path, 'rb') as f:
-        db_infos = pickle.load(f)
-    grot_range = list(grot_range)
-    if len(grot_range) == 0:
-        grot_range = None
-    sampler = DataBaseSamplerV2(db_infos, groups, db_prepor, rate, grot_range)
+    groups = [dict(group.name_to_max_num) for group in groups]
+    
+    with open(info_path, 'rb') as f: db_infos = pickle.load(f)
+    grot_range_list = list(grot_range) # List of grot range
+
+    if len(grot_range_list) == 0: grot_range_list = None
+
+    sampler = DataBaseSamplerV2(db_infos, groups, db_prepor, rate, grot_range_list)
     return sampler
